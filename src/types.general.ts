7318c2a61499fbcf58557d409d8c737901933489
Jason Kerney <Jason@TeamKerney.com>
2024-06-10 11:08:12 -0700

    refactor: removed unused fuctions

--- src/types.general.ts
+++ src/types.general.ts
@@ -16,21 +16,6 @@ export interface IFail {
 
 export type Result<T> = ISuccess<T> | IFail;
 
-export function ok<T>(successfulValue: T) : Result<T> {
-    return {
-        value: successfulValue,
-        success: true,
-    };
-};
-
-export function fail(message: string, documentPath: string) : Result<any> {
-    return {
-        message,
-        documentPath,
-        success: false,
-    };
-};
-
 export type LocationBuilder = (line: number, char: number) => ILocation;
 
 export interface IUtil {
