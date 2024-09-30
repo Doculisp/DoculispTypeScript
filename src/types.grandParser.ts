6bfc734d17c1b5e80daf95658384a08d8857a19a
Jason Kerney <Jason@TeamKerney.com>
2024-09-30 16:29:37 -0700

    feat: removed grand parser

--- src/types.grandParser.ts
+++
@@ -1,6 +0,0 @@
-import { IAst } from "./types.ast";
-import { Result } from "./types.general";
-
-export interface IGrandParser {
-    parse(documentPath: string, text: string, depth: number, index: number): Result<IAst>;
-}
