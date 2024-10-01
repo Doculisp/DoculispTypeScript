c7b4c4716c360bc8bff4082668cbcda99a3572e1
Jason Kerney <Jason@TeamKerney.com>
2024-10-01 11:33:08 -0700

    test: moving common logic to module

--- tests/parsers/document/document.test.ts
+++ tests/parsers/document/document.test.ts
@@ -6,6 +6,7 @@ import { getVerifier } from "../../tools";
 import { Options } from "approvals/lib/Core/Options";
 import fs from 'fs';
 import { IProjectLocation, Result } from "../../../src/types.general";
+import { buildLocation } from "../../testHelpers";
 
 describe('document', () => {
     let environment: ITestableContainer = undefined as any;
@@ -22,25 +23,25 @@ describe('document', () => {
     });
 
     it('should not allow a document with a zero depth.', () => {
-        const result = parse('hello', { documentPath: 'C:/my_document.md', documentDepth: 0, documentIndex: 6 });
+        const result = parse('hello', buildLocation('C:/my_document.md', 0, 6));
         
         verifyAsJson(result);
     });
 
     it('should not allow a document with a negative depth.', () => {
-        const result = parse('hello', { documentPath: 'C:/my_document.md', documentDepth: -1, documentIndex: 6 });
+        const result = parse('hello', buildLocation('C:/my_document.md', -1, 6));
         
         verifyAsJson(result);
     });
 
     it('should not allow a document with a zero index.', () => {
-        const result = parse('', { documentPath: 'C:/my_document.md', documentDepth: 4, documentIndex: 0 });
+        const result = parse('', buildLocation('C:/my_document.md', 4, 0));
         
         verifyAsJson(result);
     });
 
     it('should not allow a document with a negative index.', () => {
-        const result = parse('', { documentPath: 'C:/my_document.md', documentDepth: 4, documentIndex: -1 });
+        const result = parse('', buildLocation('C:/my_document.md', 4, -1));
         
         verifyAsJson(result);
     });
@@ -48,34 +49,34 @@ describe('document', () => {
     describe('parsing markup', () => {
         describe('text', () => {
             it('should successfully parse an empty string', () => {
-                const result = parse('', { documentPath: 'C:/my_document.md', documentDepth: 4, documentIndex: 8 });
+                const result = parse('', buildLocation('C:/my_document.md', 4, 8));
         
                 verifyAsJson(result);
             });
         
             it('should parse a simple text of "hello"', () => {
-                const result = parse('hello', { documentPath: 'C:/my_document.md', documentDepth: 3, documentIndex: 6 });
+                const result = parse('hello', buildLocation('C:/my_document.md', 3, 6));
         
                 verifyAsJson(result);
             });
     
             it('should parse text of "blow fish"', () => {
-                const result = parse('blow fish', { documentPath: 'C:/my_document.md', documentDepth: 7, documentIndex: 2});
+                const result = parse('blow fish', buildLocation('C:/my_document.md', 7, 2));
                 verifyAsJson(result);
             });
     
             it('should parse text of " blow fish"', () => {
-                const result = parse(' blow fish', { documentPath: 'C:/my_document.md', documentDepth: 4, documentIndex: 6 });
+                const result = parse(' blow fish', buildLocation('C:/my_document.md', 4, 6));
                 verifyAsJson(result);
             });
     
             it('should parse text of " blow fish "', () => {
-                const result = parse(' blow fish ', { documentPath: 'C:/my_document.md', documentDepth: 7, documentIndex: 1 });
+                const result = parse(' blow fish ', buildLocation('C:/my_document.md', 7, 1));
                 verifyAsJson(result);
             });
     
             it('should parse text of "   \\r\\n blow fish"', () => {
-                const result = parse('   \r\n blow fish', { documentPath: 'C:/my_document.md', documentDepth: 1, documentIndex: 8 });
+                const result = parse('   \r\n blow fish', buildLocation('C:/my_document.md', 1, 8));
                 verifyAsJson(result);
             });
         });
@@ -84,7 +85,7 @@ describe('document', () => {
             it('should not parse html comments', () => {
                 const md = `<!-- This is a comment -->hello bro`.trim();
     
-                const result = parse(md, { documentPath: 'C:/readme.md', documentDepth: 5, documentIndex: 2 })
+                const result = parse(md, buildLocation('C:/readme.md', 5, 2));
     
                 verifyAsJson(result);
             });
@@ -96,7 +97,7 @@ describe('document', () => {
                      \t\thello bro
                      `;
     
-                const result = parse(md, { documentPath: 'C:/readme.md', documentDepth: 8, documentIndex: 3 })
+                const result = parse(md, buildLocation('C:/readme.md', 8, 3));
     
                 verifyAsJson(result);
             });
@@ -107,7 +108,7 @@ describe('document', () => {
     
     world`;
     
-                const result = parse(md, { documentPath: 'C:/comments/helloWorld.md', documentDepth: 1, documentIndex: 2 });
+                const result = parse(md, buildLocation('C:/comments/helloWorld.md', 1, 2));
     
                 verifyAsJson(result);
             });
@@ -115,7 +116,7 @@ describe('document', () => {
             it('should parse html comments inside an inline code block', () => {
                 const md = '`<!-- an example html comment -->`';
     
-                const result = parse(md, { documentPath: 'C:/html/inline.md', documentDepth: 5, documentIndex: 3 });
+                const result = parse(md, buildLocation('C:/html/inline.md', 5, 3));
     
                 verifyAsJson(result);
             });
@@ -132,7 +133,7 @@ describe('document', () => {
     ## Sub section title
     \`\`\`
     `;
-                const result = parse(md, { documentPath: 'C:/markdown/multiline.md', documentDepth: 4, documentIndex: 3 });
+                const result = parse(md, buildLocation('C:/markdown/multiline.md', 4, 3));
     
                 verifyAsJson(result);
             });
@@ -143,7 +144,7 @@ describe('document', () => {
     World
     Boom
     `;
-                const result = parse(md, { documentPath: 'C:/examples/bad.md', documentDepth: 5, documentIndex: 4 });
+                const result = parse(md, buildLocation('C:/examples/bad.md', 5, 4));
     
                 verifyAsJson(result);
             });
@@ -151,7 +152,7 @@ describe('document', () => {
             it('should fail if inline code block does not close', () => {
                 let md = '`let b = 7';
     
-                const result = parse(md, { documentPath: 'C:/bad/noCloseInline.md', documentDepth: 8, documentIndex: 4 });
+                const result = parse(md, buildLocation('C:/bad/noCloseInline.md', 8, 4));
     
                 verifyAsJson(result);
             });
@@ -161,7 +162,7 @@ describe('document', () => {
     let b = a;
     \``;
     
-                const result = parse(md, { documentPath: 'C:/examples/badInline.md', documentDepth: 6, documentIndex: 8 });
+                const result = parse(md, buildLocation('C:/examples/badInline.md', 6, 8));
     
                 verifyAsJson(result);
             });
@@ -169,7 +170,7 @@ describe('document', () => {
             it('should fail to parse a multiline code block that does not close', () => {
                 let md = '```\nlet a = "hello;\nlet b = "world"\nconsole.log(a + " " + b);\n';
     
-                const result = parse(md, { documentPath: 'C:/bad/examples/multiline.md', documentDepth: 2, documentIndex: 7 });
+                const result = parse(md, buildLocation('C:/bad/examples/multiline.md', 2, 7));
     
                 verifyAsJson(result);
             });
@@ -179,7 +180,7 @@ describe('document', () => {
             it('should parse a doculisp block at top of file', () => {
                 const md = '<!-- (dl (# header)) -->';
     
-                const result = parse(md, { documentPath: '_main.md', documentDepth: 3, documentIndex: 7 });
+                const result = parse(md, buildLocation('_main.md', 3, 7));
     
                 verifyAsJson(result);
             });
@@ -187,7 +188,7 @@ describe('document', () => {
             it('should parse a doculisp block in the middle of file', () => {
             const md = '# Title\r\nsome text about title\r\n<!--\r\nSome lisp: (dl (# two)) -->\r\nMickey Mouse Hotline.';
     
-                const result = parse(md, { documentPath: '_main.md', documentDepth: 7, documentIndex: 5 });
+                const result = parse(md, buildLocation('_main.md', 7, 5));
     
                 verifyAsJson(result);
             });
@@ -195,7 +196,7 @@ describe('document', () => {
             it('should parse lisp outside an html tag as text', () => {
                 const md = '(# Heading) Hello Doculisp';
 
-                const result = parse(md, { documentPath: 'documentExample.md', documentDepth: 3, documentIndex: 4 });
+                const result = parse(md, buildLocation('documentExample.md', 3, 4));
 
                 verifyAsJson(result);
             });
@@ -203,7 +204,7 @@ describe('document', () => {
             it('should parse Doculisplisp outside an html tag as text', () => {
                 const md = '(dl (# Heading)) Hello Doculisp';
 
-                const result = parse(md, { documentPath: 'documentExample2.md', documentDepth: 8, documentIndex: 8 });
+                const result = parse(md, buildLocation('documentExample2.md', 8, 8));
 
                 verifyAsJson(result);
             });
@@ -229,7 +230,7 @@ describe('document', () => {
 (content (toc numbered-labeled))
 `;
 
-            let result = parse(dlisp, { documentPath: 'C:/main.dlisp', documentDepth: 7, documentIndex: 1 });
+            let result = parse(dlisp, buildLocation('C:/main.dlisp', 7, 1));
 
             verifyAsJson(result);
         });
@@ -243,7 +244,7 @@ describe('document', () => {
 (content (toc numbered-labeled)))
 `;
 
-            let result = parse(dlisp, { documentPath: 'C:/bad/extraDl.dlisp', documentDepth: 5, documentIndex: 6 });
+            let result = parse(dlisp, buildLocation('C:/bad/extraDl.dlisp', 5, 6));
 
             verifyAsJson(result);
         });
@@ -260,7 +261,7 @@ describe('document', () => {
 )
 `;
 
-            let result = parse(dlisp, { documentPath: 'C:/main.dlisp', documentDepth: 6, documentIndex: 1 });
+            let result = parse(dlisp, buildLocation('C:/main.dlisp', 6, 1));
 
             verifyAsJson(result);
         });
@@ -268,7 +269,7 @@ describe('document', () => {
         it('should handle a file with to many parenthesis', () => {
             let dlisp = `(content (toc numbered-labeled)) )`;
 
-            let result = parse(dlisp, { documentPath: 'C:/main.dlisp', documentDepth: 5, documentIndex: 8 });
+            let result = parse(dlisp, buildLocation('C:/main.dlisp', 5, 8));
 
             verifyAsJson(result);
         });
@@ -285,7 +286,7 @@ describe('document', () => {
 -->
 
 The basic structure of Doculisp is all code is contained within blocks. A block is constructed within an HTML comment region. It starts with an open parentheses \`(\`
-`, { documentPath: './structure.md', documentDepth: 2, documentIndex: 1 });
+`, buildLocation('./structure.md', 2, 1));
         
         verifyAsJson(result);
         });
@@ -316,18 +317,14 @@ Example
 \`\`\`
 
 The first block is the \`dl\` block. In it \`dl\` is the atom. It contains the \`section-meta\` sub-block.  That block has the atom \`section-meta\` followed by a further sub block. The last sub block is the \`title\` sub block. In it \`title\` is the atom and \`Basic Structure\`
-`, { documentPath: './structure.md', documentDepth: 2, documentIndex: 1 });
+`, buildLocation('./structure.md', 2, 1));
         
         verifyAsJson(result);
         });
 
         function getContent(fileName: string, depth: number, index: number): Result<DocumentMap> {
             const path: string = `./documentation/${fileName}`;
-            const location: IProjectLocation = {
-                documentDepth: depth,
-                documentIndex: index,
-                documentPath: path,
-            };
+            const location: IProjectLocation = buildLocation(path, depth, index);
 
             const content = parse(fs.readFileSync(path, { encoding: 'utf8' }), location);
             return content;
