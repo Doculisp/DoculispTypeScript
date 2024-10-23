142efe6f052c92fbdfb5e69bb08d2acfa6d9d742
Jason Kerney <Jason@TeamKerney.com>
2024-10-22 20:53:35 -0700

    feat: checking version after run

--- src/index.ts
+++ src/index.ts
@@ -4,59 +4,80 @@ import { Command, OptionValues } from 'commander';
 import { Result } from './types/types.general';
 import { IController } from './types/types.controller';
 import { IVersion } from './types/types.version';
+import Registry from '@slimio/npm-registry';
 import figlet from 'figlet';
 
 const program = new Command();
 const controller = container.buildAs<IController>('controller');
 const versionGetter = container.buildAs<IVersion>('version');
-
 const versionMaybe = versionGetter.getVersion();
 
+async function main() {
+    let version = '?.?.?'
 
-let version = '?.?.?'
+    if(!versionMaybe.success) {
+        console.log('doculisp cannot determine its version')
+    } else {
+        version = versionMaybe.value;
+    }
 
-if(!versionMaybe.success) {
-    console.log('doculisp cannot determine its version')
-} else {
-    version = versionMaybe.value;
-}
+    const helptext = '\n\n' + figlet.textSync('doculisp', {
+        font: 'Cybermedium'
+    }) + 
+    `\n            Compiler Version: ${version}` +
+    '\n            Language Version: 0.1.0' +
+    '\n';
 
-const helptext = '\n\n' + figlet.textSync('doculisp', {
-    font: 'Cybermedium'
-}) + 
-`\n            Compiler Version: ${version}` +
-'\n            Language Version: 0.1.0' +
-'\n';
+    program.addHelpText('beforeAll', helptext);
+    program.addHelpText('afterAll', '\n\n');
 
-program.addHelpText('beforeAll', helptext);
-program.addHelpText('afterAll', '\n\n');
+    program
+        .name('doculisp')
+        .description('A compiler for markdown')
+        .version(version)
+        .argument('<source>', 'the path to the file to compile')
+        .argument('<output>', 'the path to the output location including output file name')
+        .option('-t, --test', 'runs the compiler without generating the output file')
+        .action((sourcePath: string, outputPath: string, options: OptionValues) => {
+            if (options['test']) {
+                const result = controller.test(sourcePath);
+                reportResult(result);
+            }
+            else {
+                const result = controller.compile(sourcePath, outputPath);
+                reportResult(result);
+            }
+        })
+        .parseAsync(process.argv);
 
-program
-    .name('doculisp')
-    .description('A compiler for markdown')
-    .version(version)
-    .argument('<source>', 'the path to the file to compile')
-    .argument('<output>', 'the path to the output location including output file name')
-    .option('-t, --test', 'runs the compiler without generating the output file')
-    .action((sourcePath: string, outputPath: string, options: OptionValues) => {
-        if (options['test']) {
-            const result = controller.test(sourcePath);
-            reportResult(result);
+    const npmReg = new Registry();
+    const pkg = await npmReg.package("doculisp");
+
+    if(pkg.lastVersion !== version) {
+        console.log('************************************************');
+        console.log();
+        console.log('A newer version of doculisp is available!');
+        console.log();
+        console.log(`Your version: ${version}`);
+        console.log(`Latest version: ${pkg.lastVersion}`);
+        console.log();
+        console.log('Run `npm update doculisp -g` to get the latest version.');
+        console.log();
+        console.log('************************************************');
+    }
+
+
+    function reportResult(result: Result<string | false>) {
+        if (result.success) {
+            const message = !!result.value ? `Ok! ${result.value} successfully written` : 'Ok!';
+            console.log(message);
         }
         else {
-            const result = controller.compile(sourcePath, outputPath);
-            reportResult(result);
+            console.error(JSON.stringify(result, null, 4));
+            process.exit(1);
         }
-    })
-    .parse(process.argv);
-
-function reportResult(result: Result<string | false>) {
-    if (result.success) {
-        const message = !!result.value ? `Ok! ${result.value} successfully written` : 'Ok!';
-        console.log(message);
     }
-    else {
-        console.error(JSON.stringify(result, null, 4));
-        process.exit(1);
-    }
+    
 }
+
+main().catch(console.error);
