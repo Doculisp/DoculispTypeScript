60b5e16ea02582a67d7c341abfd9a44d27395f56
Jason Kerney <Jason@TeamKerney.com>
2024-11-08 06:05:13 -0800

    feat: checks version even on error

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,34 @@ const nodePath = path.dirname(path.resolve(process.argv[0] as string));
 const modulePath = path.resolve(process.argv[1] as string);
 const isCli = modulePath.includes(nodePath);
 
+async function checkVersion(version: string) {
+    const npmReg = new Registry();
+    const pkg = await npmReg.package("doculisp");
+
+    if(pkg.lastVersion !== version) {
+        console.log('************************************************');
+        console.log();
+        console.log('A newer version of doculisp is available!');
+        console.log();
+        console.log(`Your version: ${version}`);
+        console.log(`Latest version: ${pkg.lastVersion}`);
+        console.log();
+        if(isCli){
+            console.log('Run `npm update doculisp -g` to get the latest version.');
+            console.log('Or');
+            console.log('Run `doculisp --update`');
+        }
+        else {
+            console.log('Run `npm update doculisp` to get the latest version.');
+            console.log('Or');
+            console.log(`Run \`node ${modulePath} --update\``);
+        }
+        console.log();
+        console.log('************************************************');
+    }
+}
+
+
 async function main() {
     let version = '?.?.?'
 
@@ -49,56 +77,34 @@ async function main() {
             if (options['update']){
                 const updateCommand = isCli ? 'npm update doculisp -g' : 'npm update doculisp';
                 childProcess.execSync(updateCommand, { stdio: 'inherit' });
+                const updateCommand2 = isCli ? 'npm i doculisp@latest -g' : 'npm i doculisp@latest'
+                childProcess.execFileSync(updateCommand2, {stdio: 'inherit'});
                 console.log('Updated!');
                 return;
             }
             else if (options['test']) {
                 if(!sourcePath) {
                     console.log('Error: The `--test` option requires a source path.');
-                    return;
                 }
-                const result = controller.test(sourcePath);
-                reportResult(result);
+                else {
+                    const result = controller.test(sourcePath);
+                    reportResult(result);
+                }
             }
             else {
                 if(!sourcePath) {
                     console.log('Error: The source path is required.');
-                    return;
                 }
-
-                if(!outputPath) {
+                else if(!outputPath) {
                     console.log('Error: The output path is required.');
-                    return;
                 }
-
-                const result = controller.compile(sourcePath, outputPath);
-                reportResult(result);
+                else{
+                    const result = controller.compile(sourcePath, outputPath);
+                    reportResult(result);
+                }
             }
 
-            const npmReg = new Registry();
-            const pkg = await npmReg.package("doculisp");
-
-            if(pkg.lastVersion !== version) {
-                console.log('************************************************');
-                console.log();
-                console.log('A newer version of doculisp is available!');
-                console.log();
-                console.log(`Your version: ${version}`);
-                console.log(`Latest version: ${pkg.lastVersion}`);
-                console.log();
-                if(isCli){
-                    console.log('Run `npm update doculisp -g` to get the latest version.');
-                    console.log('Or');
-                    console.log('Run `doculisp --update`');
-                }
-                else {
-                    console.log('Run `npm update doculisp` to get the latest version.');
-                    console.log('Or');
-                    console.log(`Run \`node ${modulePath} --update\``);
-                }
-                console.log();
-                console.log('************************************************');
-            }
+            checkVersion(version);
         })
         .parseAsync(process.argv);
 
